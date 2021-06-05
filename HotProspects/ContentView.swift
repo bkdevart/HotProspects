b3af4b2e587da26b0d5b549d84bd6449f736392f
Brandon Knox <bknox.dev@gmail.com>
2021-06-04 18:00:23 -0700

    Minimal, user-unfriendly way of implementing tabs (have to tap grey area at bottom of screen)

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -7,35 +7,14 @@
 
 import SwiftUI
 
-class User: ObservableObject {
-    @Published var name = "Taylor Swift"
-}
-
-struct EditView: View {
-    @EnvironmentObject var user: User
-    
-    var body: some View {
-        TextField("Name", text: $user.name)
-    }
-}
-
-struct DisplayView: View {
-    @EnvironmentObject var user: User
-    
-    var body: some View {
-        Text(user.name)
-    }
-}
 
 struct ContentView: View {
-    let user = User()
     
     var body: some View {
-        VStack {
-            EditView()
-            DisplayView()
+        TabView {
+            Text("Tab 1")
+            Text("Tab 2")
         }
-        .environmentObject(user)
     }
 }
 
