486595c76118dba45a9f32a4dcab02e1f49d831f
Brandon Knox <bknox.dev@gmail.com>
2021-06-05 11:09:52 -0700

    Using blocking functions to handle whether a URLSession returns data or not

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -9,26 +9,19 @@ import SwiftUI
 
 
 struct ContentView: View {
-    @State private var selectedTab = 0
     
     var body: some View {
-        TabView(selection: $selectedTab) {
-            Text("Tab 1")
-                .onTapGesture {
-                    self.selectedTab = 1
-                }
-                .tabItem {
-                    Image(systemName: "star")
-                    Text("One")
-                }
-            
-            Text("Tab 2")
-                .tabItem {
-                    Image(systemName: "star.fill")
-                    Text("Two")
-                }
-                .tag(1)
-        }
+        Text("hello, world!")
+            .onAppear {
+                let url = URL(string: "https://www.appple.com")!
+                URLSession.shared.dataTask(with: url) { data, response, error in
+                    if data != nil {
+                        print("We got data!")
+                    } else if let error = error {
+                        print(error.localizedDescription)
+                    }
+                }.resume()
+            }
     }
 }
 
