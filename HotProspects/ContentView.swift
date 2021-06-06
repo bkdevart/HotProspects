aad5a7fb007ae2bec4289a6a608dff52e08fb03a
Brandon Knox <bknox.dev@gmail.com>
2021-06-06 10:15:46 -0700

    Example of requesting permission to give notification and scheduling one to appear (press command + L to lock simulator and see)

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -5,39 +5,36 @@
 //  Created by Brandon Knox on 6/4/21.
 //
 
+import UserNotifications
 import SwiftUI
 
 struct ContentView: View {
-    @State private var backgroundColor = Color.red
     
     var body: some View {
-        Text("Hello world")
-            .padding()
-            .background(backgroundColor)
-        
-        Text("Change Color")
-            .padding()
-            .contextMenu {
-                Button(action: {
-                    self.backgroundColor = .red
-                }) {
-                    Text("Red")
-                    Image(systemName: "checkmark.circle.fill")
-                        .foregroundColor(.red)
-                }
-                
-                Button(action: {
-                    self.backgroundColor = .green
-                }) {
-                    Text("Green")
-                }
-                
-                Button(action: {
-                    self.backgroundColor = .blue
-                }) {
-                    Text("Blue")
+        VStack {
+            Button(" Request Permission") {
+                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) {success, error in
+                    if success {
+                        print("All set!")
+                    } else if let error = error {
+                        print(error.localizedDescription)
+                    }
                 }
             }
+            
+            Button("Schedule Notification") {
+                let content = UNMutableNotificationContent()
+                content.title = "Feed the cat"
+                content.subtitle = "It looks hungry"
+                content.sound = UNNotificationSound.default
+                
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+                
+                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+                
+                UNUserNotificationCenter.current().add(request)
+            }
+        }
     }
 }
 
