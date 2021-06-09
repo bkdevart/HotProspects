88dfbe6baa85e4fa03e07faecdfd9f7316bb969a
Brandon Knox <bknox.dev@gmail.com>
2021-06-09 14:02:59 -0700

    Added static var with value of SavedData to head off any potential issues of entering the wrong value when saving UserDefaults in other views.

--- HotProspects/Prospect.swift
+++ HotProspects/Prospect.swift
@@ -17,8 +17,10 @@ class Prospect: Identifiable, Codable {
 class Prospects: ObservableObject {
     @Published var people: [Prospect]
     
+    static let saveKey = "SavedData"
+    
     init() {
-        if let data = UserDefaults.standard.data(forKey: "SavedData") {
+        if let data = UserDefaults.standard.data(forKey: Self.saveKey) {
                 if let decoded = try? JSONDecoder().decode([Prospect].self, from: data) {
                     self.people = decoded
                     return
@@ -30,7 +32,7 @@ class Prospects: ObservableObject {
     
     func save() {
         if let encoded = try? JSONEncoder().encode(people) {
-            UserDefaults.standard.set(encoded, forKey: "SavedData")
+            UserDefaults.standard.set(encoded, forKey: Self.saveKey)
         }
     }
     
